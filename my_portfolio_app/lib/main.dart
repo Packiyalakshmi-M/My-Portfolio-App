02e407e33e647c157cdbb19f99ea49520d3eb262
Packiyalakshmi-M <packiya2706.m@gmail.com>
2024-12-29 11:58:03 +0530

    Modified the orientation setting

--- my_portfolio_app/lib/main.dart
+++ my_portfolio_app/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_portfolio_app/App.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock the orientation to portrait mode
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
   runApp(const App());
 }
